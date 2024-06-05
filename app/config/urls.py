1897d192f7c00a436fef620056dd0b45691e8c2f
nicahgaturi <nicahgaturi@gmail.com>
2024-06-05 09:55:55 +0300

    modified urls.py file in the root directory

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,17 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls.conf import include
-from django.urls import path
+from django.urls import include, path
+from .views import fieldslip_create, FieldSlipDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
+    path('fieldslip/new/', fieldslip_create, name='fieldslip-create'),
+    path('fieldslip/<int:pk>/', FieldSlipDetailView.as_view(), name='fieldslip-detail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
