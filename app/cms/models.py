ddbbbebe9acde4afc6a3adf9fd2527f1b4132094
nicahgaturi <nicahgaturi@gmail.com>
2024-11-20 06:47:42 +0300

    added new models

--- app/cms/models.py
+++ app/cms/models.py
@@ -24,12 +24,11 @@ class BaseModel(models.Model):
         abstract = True
 
 
+# Locality Model
 class Locality(BaseModel):
-    abbreviation = models.CharField(
-        max_length=2, help_text="Enter the Abbreviation of the Locality"
-    )
+    abbreviation = models.CharField(max_length=2, help_text="Enter the Abbreviation of the Locality")
     name = models.CharField(max_length=50, help_text="The name of the Locality.")
-
+    
     class Meta:
         ordering = ["name"]
 
@@ -40,6 +39,7 @@ class Locality(BaseModel):
         return self.name
 
 
+# Collection Model
 class Collection(BaseModel):
     abbreviation = models.CharField(max_length=4)
     description = models.CharField(max_length=250)
@@ -51,11 +51,13 @@ class Collection(BaseModel):
         return self.description
 
 
+# Accession Model
 class Accession(BaseModel):
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE, help_text="Please select the collection")
-    specimen_prefix = models.ForeignKey(Locality, on_delete=models.CASCADE, help_text="Please select the specimen prefix"   )
+    specimen_prefix = models.ForeignKey(Locality, on_delete=models.CASCADE, help_text="Please select the specimen prefix")
     specimen_no = models.PositiveIntegerField(help_text="Please enter the specimen number")
     accessioned_by = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Please select the user who accessioned the specimen")
+
     options = (
         ('Type', 'Type'),
         ('Holotype', 'Holotype'),
@@ -67,7 +69,7 @@ class Accession(BaseModel):
         ('Neotype', 'Neotype'),
         ('Topotype', 'Topotype'),
     )
-
+    
     type_status = models.CharField(max_length=50, choices=options, null=True, blank=True, help_text="Please select the type status")
     comment = models.TextField(null=True, blank=True, help_text="Any additional comments")
 
@@ -78,6 +80,7 @@ class Accession(BaseModel):
         return f"{self.specimen_prefix}{self.specimen_no}"
 
 
+# Subject Model
 class Subject(BaseModel):
     subject_name = models.CharField(max_length=50)
 
@@ -88,6 +91,7 @@ class Subject(BaseModel):
         return self.subject_name
 
 
+# Comment Model
 class Comment(BaseModel):
     specimen_no = models.ForeignKey(Accession, on_delete=models.CASCADE, related_name='comments')
     comment = models.TextField()
@@ -100,6 +104,7 @@ class Comment(BaseModel):
         ('R', 'Rejected'),
         ('C', 'Closed'),
     )
+    
     status = models.CharField(max_length=50, choices=RESPONSE_STATUS, help_text="Please select your response")
     response = models.TextField(null=True, blank=True)
     comment_by = models.CharField(max_length=50)
@@ -111,6 +116,7 @@ class Comment(BaseModel):
         return self.comment
 
 
+# FieldSlip Model
 class FieldSlip(BaseModel):
     field_number = models.CharField(max_length=100)
     discoverer = models.CharField(max_length=255, null=True, blank=True)
@@ -122,8 +128,6 @@ class FieldSlip(BaseModel):
     verbatim_horizon = models.CharField(max_length=255, null=True, blank=True)
     verbatim_method = models.CharField(max_length=255, null=True, blank=True)
     
-        
-    # Replaced aerial_photo field with ImageField
     aerial_photo = models.ImageField(upload_to='aerial_photos/', null=True, blank=True)
 
     verbatim_latitude = models.CharField(max_length=255, null=True, blank=True)
@@ -131,7 +135,6 @@ class FieldSlip(BaseModel):
     verbatim_SRS = models.CharField(max_length=255, null=True, blank=True)
     verbatim_coordinate_system = models.CharField(max_length=255, null=True, blank=True)
     verbatim_elevation = models.CharField(max_length=255, null=True, blank=True)
-    
 
     def get_absolute_url(self):
         return reverse('fieldslip-detail', args=[str(self.id)])
@@ -139,6 +142,8 @@ class FieldSlip(BaseModel):
     def __str__(self):
         return self.field_number
 
+
+# Storage Model
 class Storage(BaseModel):
     area = models.CharField(max_length=255, blank=False, null=False)
     parent_area = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
@@ -149,13 +154,15 @@ class Storage(BaseModel):
     def __str__(self):
         return self.area
 
+
+# Reference Model
 class Reference(BaseModel):
     title = models.CharField(max_length=255, blank=False, null=False)
     first_author = models.CharField(max_length=255, blank=False, null=False)
-    year = models.PositiveIntegerField(blank=False, null=False)
+    year = models.CharField(max_length=4, blank=False, null=False)
     journal = models.CharField(max_length=255, blank=True, null=True)
-    volume = models.PositiveIntegerField(blank=True, null=True)
-    issue = models.PositiveIntegerField(blank=True, null=True)
+    volume = models.CharField(max_length=10, blank=True, null=True)
+    issue = models.CharField(max_length=10, blank=True, null=True)
     pages = models.CharField(max_length=10, blank=True, null=True)
     doi = models.CharField(max_length=255, blank=True, null=True)
     citation = models.TextField(blank=False, null=False)
@@ -165,7 +172,9 @@ class Reference(BaseModel):
 
     def __str__(self):
         return self.citation
-    
+
+
+# AccessionReference Model
 class AccessionReference(BaseModel):
     accession = models.ForeignKey(Accession, on_delete=models.CASCADE)
     reference = models.ForeignKey(Reference, on_delete=models.CASCADE)
@@ -179,3 +188,100 @@ class AccessionReference(BaseModel):
 
     def __str__(self):
         return f"{self.accession} - {self.reference}"
+
+
+# AccessionRow Model
+class AccessionRow(BaseModel):
+    accession = models.ForeignKey(Accession, on_delete=models.CASCADE)
+    storage = models.ForeignKey(Storage, on_delete=models.CASCADE)
+    specimen_suffix = models.CharField(max_length=255, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('accessionrow-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"AccessionRow {self.specimen_suffix}"
+
+
+# NatureOfSpecimen Model
+class NatureOfSpecimen(BaseModel):
+    accession_row = models.ForeignKey(AccessionRow, on_delete=models.CASCADE)
+    element = models.ForeignKey('Element', on_delete=models.CASCADE)
+    side = models.CharField(max_length=50, blank=True, null=True)
+    condition = models.CharField(max_length=255, blank=True, null=True)
+    verbatim_element = models.CharField(max_length=255, blank=True, null=True)
+    portion = models.CharField(max_length=255, blank=True, null=True)
+    fragments = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse('natureofspecimen-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"NatureOfSpecimen for AccessionRow {self.accession_row}"
+
+
+# Element Model
+class Element(BaseModel):
+    parent_element = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('element-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+
+# Person Model
+class Person(BaseModel):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    orcid = models.CharField(max_length=255, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('person-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# Identification Model
+class Identification(BaseModel):
+    accession_row = models.ForeignKey(AccessionRow, on_delete=models.CASCADE)
+    identified_by = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True, blank=True)
+    taxon = models.ForeignKey('Taxon', on_delete=models.SET_NULL, null=True, blank=True)
+    reference = models.ForeignKey(Reference, on_delete=models.SET_NULL, null=True, blank=True)
+    date_identified = models.DateField(null=True, blank=True)
+    identification_qualifier = models.CharField(max_length=255, blank=True, null=True)
+    verbatim_identification = models.CharField(max_length=255, blank=True, null=True)
+    identification_remarks = models.TextField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('identification-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"Identification for AccessionRow {self.accession_row}"
+
+
+# Taxon Model
+class Taxon(BaseModel):
+    taxon_rank = models.CharField(max_length=50)
+    kingdom = models.CharField(max_length=255)
+    phylum = models.CharField(max_length=255)
+    class_name = models.CharField(max_length=255)
+    order = models.CharField(max_length=255)
+    superfamily = models.CharField(max_length=255)
+    family = models.CharField(max_length=255)
+    subfamily = models.CharField(max_length=255)
+    tribe = models.CharField(max_length=255)
+    genus = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    infraspecific_epithet = models.CharField(max_length=255)
+    scientific_name_authorship = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('taxon-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.genus} {self.species}"
+
