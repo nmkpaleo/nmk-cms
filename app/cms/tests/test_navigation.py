72210089c9d729b52d745fcc339b1c620d1d942a
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-11-05 12:53:04 +0200

    Align navigation bar layout

--- app/cms/tests/test_navigation.py
+++ app/cms/tests/test_navigation.py
@@ -26,7 +26,8 @@ class NavigationTemplateTests(TestCase):
         response = self.client.get(reverse("index"))
 
         self.assertContains(response, _("Primary navigation"))
-        self.assertContains(response, 'class="w3-bar w3-white w3-card"', html=False)
+        self.assertContains(response, 'class="site-navigation w3-bar w3-white w3-card"', html=False)
+        self.assertContains(response, 'class="nav-items w3-hide-small"', html=False)
         self.assertContains(response, 'id="primary-navigation"', html=False)
         self.assertContains(response, 'w3-bar-block', html=False)
         self.assertContains(response, 'w3-white', html=False)
@@ -36,7 +37,8 @@ class NavigationTemplateTests(TestCase):
         self.assertContains(response, _("Login"))
         self.assertNotContains(response, _("Logout"))
         self.assertNotContains(response, 'data-dropdown-toggle="reports-menu"', html=False)
-        self.assertContains(response, 'w3-button w3-round-large w3-amber w3-text-white', html=False)
+        self.assertContains(response, 'nav-link', html=False)
+        self.assertNotContains(response, 'w3-amber', html=False)
 
     def test_navigation_structure_authenticated_regular_user(self):
         user = self._create_user("navtester")
@@ -49,8 +51,10 @@ class NavigationTemplateTests(TestCase):
         expected_user_label = _("User: %(username)s") % {"username": user.get_username()}
         self.assertContains(response, expected_user_label)
         self.assertContains(response, 'form id="logout-form"', html=False)
-        self.assertContains(response, 'w3-button w3-round-large w3-amber w3-text-white', html=False)
-        self.assertContains(response, 'w3-bar-item w3-small w3-text-dark-grey', html=False)
+        self.assertContains(response, 'class="nav-items w3-hide-small"', html=False)
+        self.assertContains(response, 'nav-link', html=False)
+        self.assertContains(response, 'nav-user-label', html=False)
+        self.assertContains(response, 'nav-auth', html=False)
         self.assertNotContains(response, 'data-dropdown-toggle="reports-menu"', html=False)
 
     def test_reports_menu_visible_for_collection_manager(self):
