ee0292d8c9025ac454b7b74a8bcc5f522241962f
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2026-09-22 10:42:59 +0300

    Handle template indexes for new QC chips

--- app/cms/static/cms/js/controllers/qc_rows_controller.js
+++ app/cms/static/cms/js/controllers/qc_rows_controller.js
@@ -82,7 +82,7 @@
     if (typeof value !== "string") {
       return value;
     }
-    var pattern = new RegExp("^" + prefix + "-\\d+-");
+    var pattern = new RegExp("^" + prefix + "-(?:\\d+|__prefix__)-");
     return value.replace(pattern, prefix + "-" + index + "-");
   }
 
@@ -90,7 +90,7 @@
     if (typeof value !== "string") {
       return value;
     }
-    var pattern = new RegExp("^" + prefix + "-\\d+-");
+    var pattern = new RegExp("^" + prefix + "-(?:\\d+|__prefix__)-");
     return value.replace(pattern, prefix + "-" + index + "-");
   }
 
@@ -602,7 +602,6 @@
       }
       container.appendChild(chip);
       this.initializeChip(chip);
-      this.assignChipToRow(chip, row);
       this.clearDeletionState(chip);
       this.refreshChipSummary(chip);
       this.updateChipIndexes(type);
