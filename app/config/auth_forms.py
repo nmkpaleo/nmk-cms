28713997192c97e2c6a4b216d726a95df84d6176
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2026-09-23 11:17:53 +0300

    Use django recaptcha module paths

--- app/config/auth_forms.py
+++ app/config/auth_forms.py
@@ -7,8 +7,8 @@ from django import forms
 from django.conf import settings
 from django.core.cache import cache
 
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 
 logger = logging.getLogger("security.auth")
